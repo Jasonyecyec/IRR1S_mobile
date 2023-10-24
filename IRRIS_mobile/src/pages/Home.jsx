cf3f3f9179ce287a8ac128e77d6fce0f8bfd8bfb
Jasonyecyec <jasonyecyec@gmail.com>
2023-10-24 12:52:19 +0800

    fix pwa

--- IRRIS_mobile/src/pages/Home.jsx
+++ IRRIS_mobile/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import HomePage from "./HomePage";
 import EyeIcon from "../assets/images/eye_icon.png";
 import ScanIcon from "../assets/images/scan_icon.png";
 import MenuIcon from "../assets/images/menu_icon.png";
+import RequestPage from "./RequestPage";
 
 const Home = () => {
   return (
@@ -14,10 +15,11 @@ const Home = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="history" element={<div>History </div>} />
           <Route path="settings" element={<Settings />} />
+          <Route path="request" element={<RequestPage />} />
         </Routes>
       </div>
 
-      <div className="bg-[#16425B] absolute py-2 bottom-20 left-1/2 transform -translate-x-1/2 rounded-lg  w-10/12  flex px-5 justify-center">
+      <div className="bg-[#16425B] absolute py-2 bottom-5 left-1/2 transform -translate-x-1/2 rounded-lg  w-10/12  flex px-5 justify-center">
         <div className="relative w-full flex justify-around ">
           <Link to="/home">
             {" "}
@@ -26,7 +28,7 @@ const Home = () => {
             </button>
           </Link>
 
-          <div className="absolute flex items-center justify-center bottom-0 right-27.5   h-full ">
+          <div className="absolute flex items-center justify-center bottom-0 right-27.5  h-full ">
             <button className="rounded-full p-1 bg-[#16425B] flex justify-center content-center">
               <img src={ScanIcon} className="w-20  h-20 object-fit" />
             </button>
--- IRRIS_mobile/vite.config.js
+++ IRRIS_mobile/vite.config.js
@@ -1,9 +1,40 @@
 import { defineConfig } from 'vite'
-import  {VitePWA} from 'vite-plugin-pwa'
-
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'AnimeVerse',
+        short_name: 'AnimeVerse',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icon_192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon_512.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+        ],
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 })
