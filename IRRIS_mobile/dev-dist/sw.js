b51041a433117ed7886e4e68f188d290a1d39657
Jasonyecyec <jasonyecyec@gmail.com>
2023-11-01 18:35:23 +0800

    fix:home icon and splashscreen

--- IRRIS_mobile/dev-dist/sw.js
+++ IRRIS_mobile/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.prru4kgbet"
+    "revision": "0.g1pnqti6sg8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
