b9fd265ba708c7ef496b321cabc38195ceb8d95e
Kurjin <euginemanliclic16@gmail.com>
2024-04-17 12:45:27 +0800

    info pencil book internal and external

--- IRRIS_mobile/src/components/ui/PencilBookInfoModal.jsx
+++ IRRIS_mobile/src/components/ui/PencilBookInfoModal.jsx
@@ -1,32 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { Button, Label, Modal, TextInput, Spinner, ModalBody } from "flowbite-react";
+import {
+  Button,
+  Label,
+  Modal,
+  TextInput,
+  Spinner,
+  ModalBody,
+} from "flowbite-react";
 
-
-const PencilBookInfoModal =({ onCloseModal })=> {
+const PencilBookInfoModal = ({ onCloseModal }) => {
   // Function to handle the close action
   const handleClose = () => {
     onCloseModal(); // Call the onCloseModal function passed from the parent component
   };
 
-    
   return (
     <>
-    <Modal
-    show
-    popup
-    size="lg"
-    onClose={handleClose}
-    className="pt-[10rem]">
+      <Modal show popup size="lg" onClose={handleClose} className="pt-[10rem]">
         <Modal.Header className="bg-mainColor ">
-          <Label className="text-white font-bold text-md"> Internal and External?</Label> 
+          <Label className="text-white font-bold text-md">
+            {" "}
+            Internal and External?
+          </Label>
         </Modal.Header>
-        <Modal.Body className="p-5">
-            lorem ipmsum
+        <Modal.Body className="p-5 flex flex-col">
+          <div className="flex flex-col  bg-gray-200 p-2 rounded-lg">
+            <Label className="text-lg">Internal Events :</Label>
+            <Label>Events organized by groups within the Campus.</Label>
+          </div>
+          <div className="flex flex-col bg-gray-200 p-2 rounded-lg mt-2">
+            <Label className="text-lg">External Events :</Label>
+            <Label>Events organized by outside groups or individuals.</Label>
+          </div>
         </Modal.Body>
-    </Modal>
-
+      </Modal>
     </>
-  )
-}
+  );
+};
 
-export default PencilBookInfoModal
+export default PencilBookInfoModal;
