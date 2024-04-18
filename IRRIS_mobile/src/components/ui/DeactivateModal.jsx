f3fa5607a52c6a6b151dadd6667956754125f063
Kurjin <euginemanliclic16@gmail.com>
2024-04-18 22:25:26 +0800

    change title to FMS instead UPKEEP

---
+++ IRRIS_mobile/src/components/ui/DeactivateModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DeactivateModal = () => {
+  return (
+    <div>DeactivateModal</div>
+  )
+}
+
+export default DeactivateModal
--- IRRIS_mobile/src/components/ui/EditProfile.jsx
+++ IRRIS_mobile/src/components/ui/EditProfile.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Button, Label, Modal, TextInput, Spinner } from "flowbite-react";
+import SuccessModal from "../SuccessModal";
+import ErrorModal from "../ErrorModal";
+import useUserStore from "@/src/services/state/userStore";
+import { getUserDetails } from "@/src/services/api/sharedService";
 
-const EditProfile=()=> {
+const EditProfile = () => {
   return (
-    <div>EditProfile</div>
-  )
-}
+    <>
+      <Modal show size="lg" popup>
+        <Modal.Header>popopop</Modal.Header>
+        <Modal.Body>edit edit</Modal.Body>
+      </Modal>
+    </>
+  );
+};
 
-export default EditProfile
+export default EditProfile;
